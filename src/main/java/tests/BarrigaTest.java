82ec60f3d914ec85575c2884a3dd5ee92f7857e1
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-06-13 21:18:57 -0300

    Deve validar campos obrigatorios na movimentacao

--- src/main/java/tests/BarrigaTest.java
+++ src/main/java/tests/BarrigaTest.java
@@ -107,5 +107,32 @@ public class BarrigaTest extends BaseTest {
 
 		;
 	}
+	
+	@Test
+	public void deveValidarCamposObrigatoriosNaMovimentacao() {
+		given()
+			.header("Authorization","JWT " + TOKEN)
+			.body("{}")
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(400)
+			.body("$", hasSize(8))
+			.body("msg", hasItems(
+					
+					"Data da Movimentação é obrigatório",
+					"Data do pagamento é obrigatório",
+					"Descrição é obrigatório",
+					"Interessado é obrigatório",
+					"Valor é obrigatório",
+					"Valor deve ser um número",
+					"Conta é obrigatório",
+					"Situação é obrigatório"
+
+					))
+		
+		;
+	}
+	
 
 }
